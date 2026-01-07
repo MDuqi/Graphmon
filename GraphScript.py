88956b26aef35c6323eb4bf7743e636adeb67530
IanLarrubia <88548918+IanLarrubia@users.noreply.github.com>
2026-01-07 16:25:53 -0300

    Add script for creating the nodes

---
+++ GraphScript.py
@@ -0,0 +1,19 @@
+import sqlite3
+import networkx as nx
+
+#create graph
+G = nx.Graph()
+Nums = []
+
+#connect to db and get all digimon numbers
+conn = sqlite3.connect('DB/Digimon.db')
+cursor = conn.cursor()
+
+Nums = cursor.execute("SELECT number FROM digimon")
+
+#add nodes to graph based on digimon numbers
+for Num in Nums:
+    G.add_node(Num[0])
+
+conn.close()
+print(G.nodes)
